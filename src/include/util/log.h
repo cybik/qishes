36b9f9bd0b1676208c382baf4d314d64377546c5
Renaud Lepage <root@cybikbase.com>
2024-07-14 23:15:18 -0700

    logger enhancement

--- src/include/util/log.h
+++ src/include/util/log.h
@@ -10,13 +10,23 @@
 
 class Log {
 public:
+    typedef enum {
+        Debug,
+        Info,
+        Warning,
+        Critical
+    } LogLevel;
     static std::shared_ptr<Log> get_logger();
     void warning(const QString& message);
     void debug(const QString& message);
     void critical(const QString& message);
     void info(const QString& message);
+    void log_level(LogLevel level);
 private:
+    void print(const QString& message);
     Log() = default;
-    static std::shared_ptr<Log> log_instance;
+    static std::shared_ptr<Log> mlogInstance;
+
+    LogLevel mlogLevel = LogLevel::Debug;
 };
 
--- src/util/log.cpp
+++ src/util/log.cpp
@@ -7,50 +7,69 @@
 #include <util/log.h>
 #include <termcolor/termcolor.hpp>
 
-std::shared_ptr<Log> Log::log_instance = nullptr;
+std::shared_ptr<Log> Log::mlogInstance = nullptr;
 
 std::shared_ptr<Log> Log::get_logger() {
-    if(!log_instance) {
-        log_instance = std::shared_ptr<Log>(new Log());
+    if(!mlogInstance) {
+        mlogInstance = std::shared_ptr<Log>(new Log());
     }
-    return log_instance;
+    return mlogInstance;
 }
-void Log::info(const QString& message) {
+
+void Log::debug(const QString& message) {
+    if(mlogLevel > LogLevel::Debug) return;
     std::cout
-            << termcolor::bold
-                << termcolor::bright_cyan  << "[INFO] "
-            << termcolor::reset
-                << termcolor::cyan << message.toStdString()
-            << termcolor::reset
-            << std::endl;
+        << termcolor::bold
+            << termcolor::bright_green
+            << "[DEBUG] "
+        << termcolor::reset
+            << termcolor::green
+            << message.toStdString()
+        << termcolor::reset
+        << std::endl;
+}
+
+void Log::info(const QString& message) {
+    if(mlogLevel > LogLevel::Info) return;
+    std::cout
+        << termcolor::bold
+            << termcolor::bright_cyan
+            << "[INFO] "
+        << termcolor::reset
+            << termcolor::cyan
+            << message.toStdString()
+        << termcolor::reset
+        << std::endl;
 }
 
 void Log::warning(const QString& message) {
+    if(mlogLevel > LogLevel::Warning) return;
     std::cout
-            << termcolor::bold
-                << termcolor::bright_yellow  << "[WARNING] "
-            << termcolor::reset
-                << termcolor::yellow << message.toStdString()
-            << termcolor::reset
-            << std::endl;
+        << termcolor::bold
+            << termcolor::bright_yellow
+            << "[WARNING] "
+        << termcolor::reset
+            << termcolor::yellow
+            << message.toStdString()
+        << termcolor::reset
+        << std::endl;
 }
-void Log::debug(const QString& message) {
-    std::cout
-            << termcolor::bold
-                << termcolor::bright_green  << "[DEBUG] "
-            << termcolor::reset
-                << termcolor::green << message.toStdString()
-            << termcolor::reset
-            << std::endl;
-}
+
 void Log::critical(const QString& message) {
+    if(mlogLevel > LogLevel::Critical) return;
     std::cout
-            << termcolor::bold
-                << termcolor::bright_red  << "[CRITICAL] "
-            << termcolor::reset
-                << termcolor::red << message.toStdString()
-            << termcolor::reset
-            << std::endl;
+        << termcolor::bold
+            << termcolor::bright_red
+            << "[CRITICAL] "
+        << termcolor::reset
+            << termcolor::red
+            << message.toStdString()
+        << termcolor::reset
+        << std::endl;
+}
+
+void Log::log_level(Log::LogLevel level) {
+    mlogLevel = level;
 }
 
 #pragma clang diagnostic pop
